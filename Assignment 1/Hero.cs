385a4124aeb9fab5067b381c5774da4cd65f4e1a
johaen11 <johngnanakumar@gmail.com>
2016-05-26 16:51:33 -0400

    Half Done Assignment 1

---
+++ Assignment 1/Hero.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+   public class Hero
+    {
+        // PRIVATE INSTANCE VARIABLES++++++++++++++++++++++++++++++
+        private int _strength;
+        private int _speed;
+        private int _health;
+        private string _name;
+
+        // PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++++++++
+        public string Name 
+            //get is when you return 
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private void _generateAbilities()
+        {
+            Random rnd = new Random();
+            _strength = rnd.Next(1, 101);
+            _speed = rnd.Next(1, 101);
+            _health = rnd.Next(1, 101);
+
+            
+
+
+        }
+
+
+        private bool _hitAttempt()
+        { Random rnd = new Random();
+
+            if (rnd.Next(1, 101) <= 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+           
+        }
+        
+        private int _hitDamage()
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(1, 7) * _strength;
+
+            
+        }
+
+        public void Fight()
+        {  
+
+            if (_hitAttempt())
+            {
+                int damage = _hitDamage();
+                Console.WriteLine("Damage hit was:" + damage);
+            }
+
+            Console.WriteLine("Hit Attempt Failed");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Strength: {0}",_strength);
+            Console.WriteLine("Health: {0}",_health);
+            Console.WriteLine("Speed: {0}",_speed);
+
+
+        }
+        
+        public Hero(string name)
+          {
+            _name = name;
+            _generateAbilities(); 
+        }
+    }
+}
--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/**
+ * Author's Name: Johaen Gnanakumar;
+ * Author’s	student	number: 300880309
+ *  Date	last	Modified:
+ * Program	description:
+ *  Revision History:
+ *  		
+ */
 namespace Assignment_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Hero hero = new Hero("Johaen");
+            hero.Show();
+
+            hero.Fight();
         }
     }
 }
